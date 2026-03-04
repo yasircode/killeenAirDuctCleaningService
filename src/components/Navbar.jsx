f50197e6213bbb0a6c927900f155b0f79b0d2527
Yasir <yasircode090@gmail.com>
2026-03-04 22:29:22 +0500

    Updated services list in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
-
 const Navbar = () => {
   const [isMobile, setIsMobile] = useState(false);
   const [showService, setShowService] = useState(false);
@@ -37,38 +36,40 @@ const Navbar = () => {
           <ul className="nav-menu-list">
             <li><Link to="/" onClick={closeMenu} className="nav-link-item">HOME</Link></li>
             
-            {/* SERVICE SECTION - CATEGORIZED AS PER CLIENT REQ */}
+            {/* SERVICE SECTION - UPDATED LIST */}
             <li className={`dropdown ${showService ? 'open' : ''}`}>
               <div className="nav-link-item" onClick={() => setShowService(!showService)}>
                 SERVICES <span className="arrow">▾</span>
               </div>
               
               <ul className="dropdown-menu mega-menu">
-                {/* Air Duct Cleaning Category */}
+                {/* Air Duct Services Category */}
                 <li className="menu-category">
-                  <span className="category-title">Air Duct Cleaning</span>
+                  <span className="category-title">Air Duct Services</span>
                   <ul>
-                    <li><Link to="/HVACInstallation" onClick={closeMenu}>HVAC System Cleaning</Link></li>
-                    <li><Link to="/dryerventcleaning" onClick={closeMenu}>Dryer Vent Cleaning</Link></li>
-                    <li><Link to="/chimneycleaning" onClick={closeMenu}>Chimney Sweep</Link></li>
+                    <li><Link to="/air-duct-cleaning" onClick={closeMenu}>Air Duct Cleaning</Link></li>
+                    <li><Link to="/air-duct-repair" onClick={closeMenu}>Air Duct Repair</Link></li>
+                    <li><Link to="/air-duct-replacement" onClick={closeMenu}>Air Duct Replacement</Link></li>
+                    <li><Link to="/hvac-cleaning" onClick={closeMenu}>HVAC System Cleaning</Link></li>
                   </ul>
                 </li>
 
-                {/* Chimney Services Category */}
+                {/* Chimney & Vent Category */}
                 <li className="menu-category">
-                  <span className="category-title">Chimney Services</span>
+                  <span className="category-title">Chimney & Vents</span>
                   <ul>
-                    <li><Link to="/airductcleaning" onClick={closeMenu}>Air Duct Repair</Link></li>
-
+                    <li><Link to="/dryer-vent-cleaning" onClick={closeMenu}>Dryer Vent Cleaning</Link></li>
+                    <li><Link to="/chimney-sweep" onClick={closeMenu}>Chimney Sweep</Link></li>
+                    <li><Link to="/chimney-services" onClick={closeMenu}>Chimney Services</Link></li>
                   </ul>
                 </li>
 
-                {/* Replacement & More Category */}
+                {/* Insulation & Purification Category */}
                 <li className="menu-category">
-                  <span className="category-title">Air Duct Replacement</span>
+                  <span className="category-title">Home Comfort</span>
                   <ul>
-                    <li><Link to="/atticinsulation" onClick={closeMenu}>Attic Insulation</Link></li>
-                    <li><Link to="/uvlightinstallation" onClick={closeMenu}>Indoor Air UV Light Purification</Link></li>
+                    <li><Link to="/attic-insulation" onClick={closeMenu}>Attic Insulation</Link></li>
+                    <li><Link to="/uv-light-purification" onClick={closeMenu}>Indoor Air UV Light Purification</Link></li>
                   </ul>
                 </li>
               </ul>
@@ -81,23 +82,18 @@ const Navbar = () => {
               </div>
               <ul className="dropdown-menu scrollable-menu">
                 <li><Link to="/location/killeen" onClick={closeMenu}>Killeen, TX</Link></li>
-                 <li><Link to="/location/harkerheights" onClick={closeMenu}>Harker Heights, TX</Link></li>
-                   <li><Link to="/location/copperascove" onClick={closeMenu}>Copperas Cove, TX</Link></li>
-                    <li><Link to="/location/Nolanville" onClick={closeMenu}>Nolanville, TX</Link></li>
-                      <li><Link to="/location/fortcavazos" onClick={closeMenu}>FortCavaos, TX</Link></li>
-                       <li><Link to="/location/belton" onClick={closeMenu}>Belton, TX</Link></li>
+                <li><Link to="/location/harkerheights" onClick={closeMenu}>Harker Heights, TX</Link></li>
+                <li><Link to="/location/copperascove" onClick={closeMenu}>Copperas Cove, TX</Link></li>
+                <li><Link to="/location/Nolanville" onClick={closeMenu}>Nolanville, TX</Link></li>
+                <li><Link to="/location/fortcavazos" onClick={closeMenu}>FortCavaos, TX</Link></li>
+                <li><Link to="/location/belton" onClick={closeMenu}>Belton, TX</Link></li>
                 <li><Link to="/location/temple" onClick={closeMenu}>Temple, TX</Link></li>
-                  <li><Link to="/location/waco" onClick={closeMenu}>Waco, TX</Link></li>
-                  
+                <li><Link to="/location/waco" onClick={closeMenu}>Waco, TX</Link></li>
                 <li><Link to="/location/libertyhill" onClick={closeMenu}>Liberty Hill, TX</Link></li>
                 <li><Link to="/location/jarrell" onClick={closeMenu}>Jarrell, TX</Link></li>
-              
-               
-             <li><Link to="/location/salado" onClick={closeMenu}>Salado, TX</Link></li>
+                <li><Link to="/location/salado" onClick={closeMenu}>Salado, TX</Link></li>
                 <li><Link to="/location/georgetown" onClick={closeMenu}>Georgetown, TX</Link></li>
-                 <li><Link to="/location/lampasas" onClick={closeMenu}>Lampasas, TX</Link></li>
-               
-                {/* ... other locations */}
+                <li><Link to="/location/lampasas" onClick={closeMenu}>Lampasas, TX</Link></li>
               </ul>
             </li>
             
