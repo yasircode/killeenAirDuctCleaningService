20c9f593bc63c3a14399521ab70cb7d4ee54ea30
Yasir <yasircode090@gmail.com>
2026-04-04 13:34:59 +0500

    new map updated

--- src/DuctLandingPage.jsx
+++ src/DuctLandingPage.jsx
@@ -95,7 +95,7 @@ const DuctLandingPage = () => {
             {/* Map Wrapper with 100% width and clickable Overlay */}
             <div className="map-wrapper" style={{ overflow: 'hidden', borderRadius: '8px', position: 'relative', height: '180px' }}>
               <iframe 
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d110143.12569527712!2d-97.80785165!3d31.1171194!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8645494883307777%3A0x6335a16d764786!2sKilleen%2C%20TX!5e0!3m2!1sen!2sus!4v1710000000000!5m2!1sen!2sus" 
+                src="https://share.google/BMfj0RfUDFbUyYZxN" 
                 width="100%" 
                 height="100%" 
                 style={{ border: 0 }}
